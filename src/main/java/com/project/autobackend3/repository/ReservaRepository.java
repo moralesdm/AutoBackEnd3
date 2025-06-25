fe506bd1b8e6203003415d7f704b05908ff9df72
moralesdm <morales.david.gr.22@gmail.com>
2025-06-24 23:31:40 -0500

    feat(repository): add ReservaRepository with query by usuarioId

--- src/main/java/com/project/autobackend3/repository/ReservaRepository.java
+++ src/main/java/com/project/autobackend3/repository/ReservaRepository.java
@@ -1,4 +1,9 @@
 package com.project.autobackend3.repository;
 
-public class ReservaRepository {
+import com.project.autobackend3.entity.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    List<Reserva> findByUsuarioId(Long usuarioId);
 }
